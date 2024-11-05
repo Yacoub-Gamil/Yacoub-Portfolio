4cade21afbd06d73e2cb9c9e528459b14cd7b697
yacoub-gamil <yacoub-gamil@hotmail.com>
2024-11-05 14:51:44 +0200

    added contact and some element

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import { BrowserRouter } from "react-router-dom";
 import AppLayout from "./ui/AppLayout";
+import { Toaster } from "react-hot-toast";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <AppLayout />
-    </BrowserRouter>
+    <>
+      <Toaster
+        containerStyle={{
+          top: 80,
+          left: 0,
+          bottom: 20,
+          right: 20,
+        }}
+        reverseOrder={false}
+      />
+      <BrowserRouter>
+        <AppLayout />
+      </BrowserRouter>
+    </>
   );
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,50 @@
 import { useState } from "react";
 import { HiMiniDevicePhoneMobile, HiMiniEnvelope } from "react-icons/hi2";
 import { IoIosSend, IoMdSend } from "react-icons/io";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import toast from "react-hot-toast";
 
 function Contact() {
   const [show, setShow] = useState(false);
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm("service_4z2tr69", "template_1vuyb2h", form.current, {
+        publicKey: "kb4a3KO8I95-j9F25",
+      })
+      .then(
+        () => {
+          toast.success(
+            "Successfully sent!",
+            {
+              icon: "👍",
+              style: {
+                borderRadius: "10px",
+                border: "1px solid #fff",
+                background: "#121212",
+                color: "#fff",
+                padding: "10px",
+              },
+            },
+            {}
+          );
+          document.getElementById("myForm").reset();
+        },
+        () => {
+          toast.error("Email not sent!", {
+            icon: "❌",
+            style: {
+              borderRadius: "10px",
+              background: "#333",
+              color: "#fff",
+            },
+          });
+        }
+      );
+  };
   return (
     <section className=" mb-[4rem] p-4 container">
       <div id="contact" className=" mt-[4rem]">
@@ -14,23 +55,31 @@ function Contact() {
           Contact
         </h1>
       </div>
-      <form className=" w-full  mt-[3rem] flex flex-col items-center">
+      <form
+        id="myForm"
+        ref={form}
+        onSubmit={sendEmail}
+        className=" w-full  mt-[3rem] flex flex-col items-center"
+      >
         <div className=" xs:w-[20rem] sm:w-[30rem] md:w-[40rem] lg:w-[45rem] xl:w-[60rem]  text-white gap-6 p-4 flex flex-col rounded-xl  shadow-[0px_0px_10px_0px_#5a605e]">
           <input
+            name="from_name"
             required
             placeholder="Name"
             type="text"
             className=" outline-none text-black xs:p-2 xl:p-3  font-semibold  rounded-xl"
           />
           <input
+            name="from_email"
             required
             type="email"
             placeholder="Email@example.com"
             className=" xl:p-3 rounded-xl outline-none xs:p-2 font-semibold  text-black "
           />
           <textarea
+            name="message"
             maxLength={1000}
-            minLength={100}
+            minLength={10}
             placeholder="Message"
             required
             className=" xl:p-3 rounded-xl outline-none xs:p-2 font-semibold  text-black "
@@ -65,6 +114,19 @@ function Contact() {
       </footer>
     </section>
   );
+
+  // return (
+  //   <form >
+  //     <label>Name</label>
+  //     <input type="text" name="from_name" />
+  //     <label>Email</label>
+  //     <input type="email" name="from_email" />
+  //     <label>Message</label>
+  //     <textarea name="message" />
+  //     <input type="submit" value="Send" />
+  //   </form>
+  // );
+  // );
 }
 
 export default Contact;
